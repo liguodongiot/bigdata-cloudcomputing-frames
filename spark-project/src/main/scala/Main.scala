abdcfaa5b59006b1e94c763467c8f06454a034d5
liguodong <liguodong@cd.tuan800.com>
2017-02-22 19:54:09 +0800

    DateTime时间处理

--- spark-project/src/main/scala/Main.scala
+++ spark-project/src/main/scala/Main.scala
@@ -1,15 +1,31 @@
+import java.text.SimpleDateFormat
+
+import org.apache.commons.lang3.time.FastDateFormat
+import org.joda.time.DateTime
+
 /**
   * Created by liguodong on 2016/12/24.
   */
 object Main extends App{
   println("hello,world...")
 
+  //单词统计
   val lines = List("hello tom hello jerry", "hello jerry", "hello kitty")
   println(lines.flatMap(_.split(" ")).map((_,1)).groupBy(_._1).mapValues(_.foldLeft(0)(_+_._2)))
   lines.flatMap(_.split(" ")).map((_,1)).groupBy(_._1).map(t=>(t._1,t._2.size))
   lines.flatMap(_.split(" ")).map((_,1)).groupBy(_._1).mapValues(_.foldLeft(0)(_+_._2))
 
+  //时间处理
+  val datetime = new DateTime(2017,1,1,0,0,0)
+  println(datetime)
+  val dateFormat = FastDateFormat.getInstance("yyyy年MM月dd日,E,HH:mm:ss")
+  val logTime = dateFormat.parse("2016年2月1日,星期一,10:01:37").getTime
+  println(logTime)
+  val datelong = new DateTime(logTime)
+  println(datelong.toString("yyyyMMdd"))
+  println(datelong.toString("yyyy年MM月dd日,E,HH:mm:ss"))
 
-
-
+  val simpleDateFormat = new SimpleDateFormat("yyyy年MM月dd日,E,HH:mm:ss")
+  val logTime2 = simpleDateFormat.parse("2016年2月1日,星期一,10:01:37").getTime
+  println(new DateTime(logTime2).toString("yyyy年MM月dd日,E,HH:mm:ss"))
 }
--- spark-project/src/main/scala/com/lgd/spark/quota/GameKPI.scala
+++ spark-project/src/main/scala/com/lgd/spark/quota/GameKPI.scala
@@ -21,6 +21,7 @@ object GameKPI {
     //过滤后并缓冲
     val filteredLogs = splitedLogs.filter(fields => FilterUtils.filterByTime(fields, beginTime, endTime))
       .cache()
+
     //日新增用户数，Daily New Users 缩写 DNU
     val dnu = filteredLogs.filter(fields => FilterUtils.filterByType(fields, EventType.REGISTER))
       .count()
@@ -36,13 +37,14 @@ object GameKPI {
     //留存率: 某段时间的新增用户数记为A，经过一段时间后，仍然使用的用户占新增用户A的比例即为留存率
     //次日留存率: 日新增用户在+1日登陆的用户占新增用户的比例
     val t1 = TimeUtils.getCertainDayTime(-1)
+
     val lastDayRegUser = splitedLogs.filter(fields => FilterUtils.filterByTypeAndTime(fields, EventType.REGISTER, t1, beginTime))
       .map(x => (x(3), 1))
+
     val todayLoginUser = filteredLogs.filter(fields => FilterUtils.filterByType(fields, EventType.LOGIN))
       .map(x => (x(3), 1))
       .distinct()
-
-
+    
     val d1r: Double = lastDayRegUser.join(todayLoginUser).count()
     println("次日留存用户数："+d1r)
 
